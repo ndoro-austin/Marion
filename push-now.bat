fe253dcd5cd0a3b05627d5ef172009beb36f2bf8
Pwavy <austinndoro03@gmail.com>
2026-01-12 06:57:10 +0300

    Update: Remove Contact link from navbar on all pages

---
+++ push-now.bat
@@ -0,0 +1,15 @@
+@echo off
+cd /d "c:\Users\Austin\OneDrive\Desktop\kaisa"
+
+echo Adding all changes...
+git add .
+
+echo Creating commit...
+git commit -m "Update: Remove Contact link from navbar on all pages"
+
+echo Pushing to GitHub...
+git push
+
+echo.
+echo Done! All changes pushed to GitHub.
+pause
